19136572d34d445e0b588a238c34b362d7ea821f
Daviafer <daviafer1@gmail.com>
2022-06-11 18:48:32 -0300

    Finalizando estudos

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Fundamentos do JavaScript
+
+Nesse estudos básicos foram abordados:
+- Fundamentos 
+- Declaração de variáveis
+- Tipos de dados
+- String
+- Array
+- Funções
+- Objetos
+- Dom
--- scripts.js
+++ scripts.js
@@ -80,46 +80,41 @@
 //////////////////////////////////////////
 // // objetos
 //////////////////////////////////////////
-const product = {
-  name: 'Camisa',
-  price: 15.99,
-  inStock: true,
-  sizes: ['P', 'M', 'G'],
-}
+// const product = {
+//   name: 'Camisa',
+//   price: 15.99,
+//   inStock: true,
+//   sizes: ['P', 'M', 'G'],
+// }
 // console.log(product['name'])
-console.log(product.name) // acessando name. Resultado: Camisa
+// console.log(product.name) // acessando name. Resultado: Camisa
 
-product.color = 'Azul' // Adicionando propriedade
+// product.color = 'Azul' // Adicionando propriedade
 
-console.log(product)
+// console.log(product)
 
 //////////////////////////////////////////
 // // destructuring
 //////////////////////////////////////////
-const { price, inStock } = product 
+// const { price, inStock } = product 
 
-console.log(price)
-console.log(inStock)
+// console.log(price)
+// console.log(inStock)
 
 //////////////////////////////////////////
 // // JSON
 //////////////////////////////////////////
-// const dog = {
-//   name: 'Shark',
-//   age: 10,
-// }
-
-// const json = JSON.stringify(dog)
-
+// const dog = {name: 'Shark',age: 10,} // JS
+// const json = JSON.stringify(dog)  // convertendo pra Json
 // console.log(json)
 
-// const obj = JSON.parse(json)
-
+// const obj = JSON.parse(json)  // do Json pro JS
 // console.log(obj)
 
+//////////////////////////////////////////
 // // Estruturas condiconais
+//////////////////////////////////////////
 // const a = 10
-
 // if (a > 8) {
 //   console.log('A é maior que 8!')
 // }
@@ -128,125 +123,151 @@ console.log(inStock)
 
 // if (b === 'João') {
 //   console.log('O nome é João!')
-// } else if (b === 'Pedro') {
-//   console.log('O nome é Pedro!')
+// } else if (b === 'Davi') {
+//   console.log('O nome é Davi!')
 // } else {
 //   console.log('Não encontramos o nome do usuário!')
 // }
-
+// // IF ternário
 // const someNumber = 14
-
 // let testingANumber = someNumber < 20 ? 'Yes' : 'No'
 
-// console.log(testingANumber) // No
+// console.log(testingANumber) // yes
 
+//////////////////////////////////////////
 // // Estruturas de repetição
+//////////////////////////////////////////
 // const myList = [1, 2, 3, 4, 5]
 // let counter = 0
-
 // while (counter < myList.length) {
 //   console.log('Imprimindo: ' + myList[counter])
 //   counter++
 // }
 
 // const mySecondList = ['a', 'b', 'c', 'd', 'e']
-
 // for (let counter = 0; counter < mySecondList.length; counter++) {
 //   console.log('Imprimindo: ' + mySecondList[counter])
 // }
-
+//////////////////////////////////////////
 // // array methods
+//////////////////////////////////////////
+//forEach
 // const names = ['Davi', 'João', 'Pedro', 'Maria']
 
 // names.forEach(function (name) {
 //   console.log(`O nome é: ${name}`)
 // })
 
+// MAP 
 // const modifiedNames = names.map(function (name) {
 //   if (name === 'Davi') {
-//     return (name = 'Sr. Davi')
+//     return (name = 'Sr. Davi')  // alterando Davi do array pra Sr Davi
 //   } else {
 //     return name
 //   }
 // })
-
 // console.log(modifiedNames)
 
+//Filter
 // const bigNumbers = [1, 2, 3, 10, 50].filter((number) => {
 //   return number > 5
 // })
-
 // console.log(bigNumbers) // [10, 50]
 
+// // Reduce
 // const sumAll = [10, 20, 30, 40, 50].reduce((total, number) => {
-//   return total + number
+//   return total + number // pegando 10, volta e soma com 20, volta e soma com 30 ...
 // })
+// console.log(sumAll) // 150  - total da união
 
-// console.log(sumAll) // 150
-
-// // Funções
+//////////////////////////////////////////
+// Funções
+//////////////////////////////////////////
 // function minhaFuncao() {
 //   console.log('Olá função!')
 // }
-
-// minhaFuncao()
+// minhaFuncao() // mostrando 
 
 // function nomeCompleto(nome, sobrenome) {
 //   return `Olá ${nome} ${sobrenome}, como vai?`
 // }
-
-// const saudacao = nomeCompleto('Davi', 'Fer')
-
+// const saudacao = nomeCompleto('Davi', 'Fer') // passando parâmetros da função (nome, sobrenome)
 // console.log(saudacao)
 
+// const primeiroNome = 'Primeiro'
+// const segundoNome = 'Segundo'
+// const primeiroSegundoNome = nomeCompleto(primeiroNome, segundoNome) 
+// console.log(primeiroSegundoNome)
+
+// arrow function
 // const myArrowFunction = (a, b) => {
 //   return a + b
 // }
+// console.log(myArrowFunction(2, 2))  // colocando valores nos parâmetros da arrow function 
 
-// console.log(myArrowFunction(2, 2))
-
-// const mySimpleArrow = (a, b) => a + b
-
+// const mySimpleArrow = (a, b) => a + b   // simplificada
 // console.log(mySimpleArrow(5, 5))
 
+//////////////////////////////////////////
 // // classes
+//////////////////////////////////////////
 // class Product {
-//   constructor(name, price) {
+//   constructor(name, price, size) {
 //     this.name = name
 //     this.price = price
 //   }
-
 //   productDetails() {
-//     return `O nome do produto é: ${this.name} e ele custa RS${this.price}`
+//     return `O nome do produto é ${this.name} e custa RS ${this.price} `
 //   }
 // }
-
 // const socks = new Product('Meia branca', 5.99)
-
 // console.log(socks.productDetails())
 
+// Herança  // colocando um atributo específico para o objeto
+// class SuperProduct extends Product {
+//   constructor(name, price, size) {
+//     super(name, price)
+//     this.size = size
+//   }
+//   showAdjective(adjective){
+//     return `O ${this.name} é muito ${adjective}`
+//   }
+// }
+// const tenis = new SuperProduct('Tênis verde', 50.90, '42')
+// console.log('Tipo: ' + tenis.name +'. Tamanho do tênis: ' + tenis.size)
+// console.log(tenis.showAdjective('Elegante'))
+
+//////////////////////////////////////////
 // // DOM
-// const title = document.getElementById('title')
-// const sameTitle = document.querySelector('#title')
+//////////////////////////////////////////
+const title = document.getElementById('title')
+console.log(title)
+const sameTitle = document.querySelector('#title')
+console.log(sameTitle)
+const texts = document.querySelectorAll('.text')
+console.log(texts)
+console.log(texts[1]) // selecionando elemento específico
 
-// const texts = document.querySelectorAll('.text')
+// acessando texts com toddas as classes e alterando 
+texts.forEach((textComForEach) => { 
+  console.log(textComForEach.textContent.toUpperCase())
+})
 
-// texts.forEach((text) => {
-//   console.log(text.textContent.toUpperCase())
-// })
+// manipulação de elementos
+title.textContent = 'Mudei o texto'
 
-// // manipulação de elementos
-// title.textContent = 'Mudei o texto'
+texts[0].innerHTML = '<h3>Alteramos o HTML deste elemento</h3>'
 
-// texts[0].innerHTML = '<span>Alteramos o HTML deste elemento</span>'
+texts[1].style.backgroundColor = 'red'  // colocando estilo
 
-// texts[1].style.backgroundColor = 'red'
+texts[2].classList.add('my-class')  // adicionando classe
 
-// texts[2].classList.add('my-class')
+texts[3].remove()
 
-// texts[3].remove()
-
-// // eventos
-// btn.addEventListener('click', function () {
-//   console.log('clicou!')
-// })
+// eventos
+btn.addEventListener('click', function () {
+  console.log('clicou!')
+})
+btn.addEventListener('click', function () {
+  texts[2].style.color = 'blue'
+})
